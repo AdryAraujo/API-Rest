e15165fb11411d2db1f3234dd3c085807e3e6be6
Adryelle Silva <191036532940@academico.ufs.br>
2024-01-18 11:12:12 -0300

    crud simples express

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-const express = require("express");
-const expressSession = require("express-session");
-const morgan = require("morgan");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-require("dotenv-safe").config();
-require("./config/db");
+import { response } from "express";
+import app from "./app.js"
 
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors({ origin: "*" }));
-app.use(morgan("dev"));
-app.use(
-  expressSession({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.listen(process.env.PORT, () => {
-  console.log("server started at localhost:4000");
+app.listen(3000, () => {
+  console.log("server started");
 });
