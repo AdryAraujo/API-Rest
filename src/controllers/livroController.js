f517a335ada0352b9cd23f5bce0bbcf6bb27c2e8
Adryelle Silva <191036532940@academico.ufs.br>
2024-01-22 11:09:07 -0300

    alterações

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/livros.js";
 
 class LivroController {
@@ -22,8 +23,10 @@ class LivroController {
     }
 
     static async cadastrarLivro (req, res){
+      const novoLivro = req.body;
       try{
-        const novoLivro = await livro.create(req.body);
+        const autorEncontrado = await autor.findById(novoLivro.autor)
+        const livroCompleto = {...novoLivro, autor: {...autorEncontrado._doc}}
         res.status(201).json({message:"criado com sucesso", livro: novoLivro})
       }catch(erro){
         res.status(500).json({message: `${erro.message} : Falha ao cadastrar o livro`})
